b697258f1c2c4c209465a60409b6b7ae137bc7e4
Alex Black <wa7@sa34.cx>
2021-12-29 18:07:50 +0300

    json

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 async function temp() {
   let res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   console.log(res.status);
+  let json = await res.json();
+  console.log(json);
   // .then((response) => response.json())
   // .then((json) => console.log(json));
 }
